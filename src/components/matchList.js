0d1c9e4a39b150fc4e265d4910b0ee42150a5327
Rachel PR <rplourder@gmail.com>
2022-05-09 20:23:52 -0300

    Create componenets

---
+++ src/components/matchList.js
@@ -0,0 +1,10 @@
+function MatchList() {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {/* Matches will be shown here */}
+    </section>
+  );
+}
+
+export default MatchList;
---
+++ src/components/playerList.js
@@ -0,0 +1,10 @@
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {/* Players will be shown here */}
+    </section>
+  );
+}
+
+export default PlayerList;
